b33e18f886c8a23b5afb3d04b76f53b1b77974c4
vikramft <vikram.singh@forethought.in>
2018-12-21 11:41:53 +0530

    url change by vk

--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,7 @@ $router->get('/', function () use ($router) {
 $router->group(['prefix'=>'api/v1'], function() use($router){
 
 //start account router
-$router->get('/accounts', 'CRM\AccountController@index');
+$router->get('/account', 'CRM\AccountController@index');
 $router->post('/account', 'CRM\AccountController@store');
 $router->get('/account/{id}', 'CRM\AccountController@show');
 $router->put('/account/{id}', 'CRM\AccountController@update');
@@ -28,7 +28,7 @@ $router->delete('/account/{id}', 'CRM\AccountController@destroy');
 //end account router
 
 //start lead router
-$router->get('/leads', 'CRM\LeadController@index');
+$router->get('/lead', 'CRM\LeadController@index');
 $router->post('/lead', 'CRM\LeadController@store');
 $router->get('/lead/{id}', 'CRM\LeadController@show');
 $router->put('/lead/{id}', 'CRM\LeadController@update');
@@ -36,7 +36,7 @@ $router->delete('/lead/{id}', 'CRM\LeadController@destroy');
 //end lead router
 
 //start cmapaign router
-$router->get('/campaigns', 'CRM\CampaignController@index');
+$router->get('/campaign', 'CRM\CampaignController@index');
 $router->post('/campaign', 'CRM\CampaignController@store');
 $router->get('/campaign/{id}', 'CRM\CampaignController@show');
 $router->put('/campaign/{id}', 'CRM\CampaignController@update');
@@ -44,7 +44,7 @@ $router->delete('/campaign/{id}', 'CRM\CampaignController@destroy');
 //end campaign router
 
 //start contact router
-$router->get('/contacts', 'CRM\ContactController@index');
+$router->get('/contact', 'CRM\ContactController@index');
 $router->post('/contact', 'CRM\ContactController@store');
 $router->get('/contact/{id}', 'CRM\ContactController@show');
 $router->put('/contact/{id}', 'CRM\ContactController@update');
@@ -52,7 +52,7 @@ $router->delete('/contact/{id}', 'CRM\ContactController@destroy');
 //end contact router
 
 //start oppertiunity router
-$router->get('/opportunities', 'CRM\OpportunityController@index');
+$router->get('/opportunity', 'CRM\OpportunityController@index');
 $router->post('/opportunity', 'CRM\OpportunityController@store');
 $router->get('/opportunity/{id}', 'CRM\OpportunityController@show');
 $router->put('/opportunity/{id}', 'CRM\OpportunityController@update');
@@ -60,7 +60,7 @@ $router->delete('/opportunity/{id}', 'CRM\OpportunityController@destroy');
 //end oppertiunity router
 
 //start customer router
-$router->get('/customers', 'CRM\CustomerController@index');
+$router->get('/customer', 'CRM\CustomerController@index');
 $router->post('/customer', 'CRM\CustomerController@store');
 $router->get('/customer/{id}', 'CRM\CustomerController@show');
 $router->put('/customer/{id}', 'CRM\CustomerController@update');
